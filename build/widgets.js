71762533af60cb77a323bd22365b7d661fceac74
Dallin Scherbel <scherpa.bball@gmail.com>
2014-11-12 22:27:37 -0700

    added more events to upload widget

--- build/widgets.js
+++ build/widgets.js
@@ -487,11 +487,44 @@
             widget.on("show", function() {
                 widget.show();
             });
-            widget.on("ready", function() {
-                exports.fire(name + "::ready", this);
-            });
             widget.on("destroyed", function() {
                 exports.fire(name + "::destroyed", this);
+            });
+            widget.on("uploadReady", function(evt, data) {
+                exports.fire(name + "::ready", this, data);
+            });
+            widget.on("uploadActive", function(evt, data) {
+                exports.fire(name + "::active", this, data);
+            });
+            widget.on("uploadFileAdded", function(evt, data) {
+                exports.fire(name + "::fileAdded", this, data);
+            });
+            widget.on("uploadStart", function(evt, data) {
+                exports.fire(name + "::start", this, data);
+            });
+            widget.on("uploadStop", function(evt, data) {
+                exports.fire(name + "::stop", this, data);
+            });
+            widget.on("uploadSubmit", function(evt, data) {
+                exports.fire(name + "::submit", this, data);
+            });
+            widget.on("uploadUserAborted", function(evt, data) {
+                exports.fire(name + "::userAborted", this, data);
+            });
+            widget.on("uploadSuccess", function(evt, data) {
+                exports.fire(name + "::success", this, data);
+            });
+            widget.on("uploadFailed", function(evt, data) {
+                exports.fire(name + "::failed", this, data);
+            });
+            widget.on("uploadPost", function(evt, data) {
+                exports.fire(name + "::post", this, data);
+            });
+            widget.on("uploadPostSuccess", function(evt, data) {
+                exports.fire(name + "::postSuccess", this, data);
+            });
+            widget.on("uploadPostError", function(evt, data) {
+                exports.fire(name + "::postError", this, data);
             });
         };
     })();
